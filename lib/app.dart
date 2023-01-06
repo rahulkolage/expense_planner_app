cb173e2c6fd19b31621b350d8af1c1f10ae8410c
Rahul Kolage <rahulkolage@gmail.com>
2023-01-06 16:35:08 +0530

    MediaQuery added to calculate dynamic height

--- lib/app.dart
+++ lib/app.dart
@@ -75,8 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    final appBar = AppBar(
         title: const Text(
           'Personal Expenses',
         ),
@@ -86,14 +85,22 @@ class _MyHomePageState extends State<MyHomePage> {
             icon: Icon(Icons.add),
           )
         ],
-      ),
+      );
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.start, // it's default
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Chart(_recentTransactions),
-            TransactionList(_userTransactions, _deleteTransaction),
+            Container(
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height
+              - MediaQuery.of(context).padding.top) * 0.4,
+              child: Chart(_recentTransactions)),
+            Container(
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height
+              - MediaQuery.of(context).padding.top) * 0.6,
+              child: TransactionList(_userTransactions, _deleteTransaction)),
           ],
         ),
       ),
